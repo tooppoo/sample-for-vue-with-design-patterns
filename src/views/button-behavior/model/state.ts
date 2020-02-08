32bcf3d0675c5e11a12ba486ca566c0f3f68a407
philomagi <tooppoo56@gmail.com>
2020-02-09 02:36:47 +0900

    Stateオブジェクト化

--- src/views/button-behavior/model/state.ts
+++ src/views/button-behavior/model/state.ts
@@ -1,20 +1,33 @@
 type LoadingStateValue = 'loading' | 'success' | 'failed'
 export interface LoadingState {
-  label: string
-  value: LoadingStateValue
+  readonly label: string
+  readonly value: LoadingStateValue
+  readonly isActivated: boolean
+
+  activate(): LoadingState
+  inactivate(): LoadingState
+}
+
+class BaseLoadingState implements LoadingState {
+  static initialize (label: string, value: LoadingStateValue): LoadingState {
+    return new BaseLoadingState(label, value, false)
+  }
+  private constructor (
+    readonly label: string,
+    readonly value: LoadingStateValue,
+    readonly isActivated: boolean
+  ) { }
+
+  activate (): LoadingState {
+    return new BaseLoadingState(this.label, this.value, true)
+  }
+  inactivate (): LoadingState {
+    return new BaseLoadingState(this.label, this.value, false)
+  }
 }
 
 export const LoadingStateList: LoadingState[] = [
-  {
-    label: 'Loading',
-    value: 'loading'
-  },
-  {
-    label: 'Success',
-    value: 'success'
-  },
-  {
-    label: 'Failed',
-    value: 'failed'
-  }
+  BaseLoadingState.initialize('Loading', 'loading').activate(),
+  BaseLoadingState.initialize('Success', 'success'),
+  BaseLoadingState.initialize('Failed', 'failed')
 ]
