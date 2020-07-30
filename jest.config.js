e1f6b29a8f720cbbbbe234c64b9cb721254a3bc0
philomagi <tooppoo56@gmail.com>
2020-07-30 23:02:42 +0900

    jest追加

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: [
+    '<rootDir>/spec/**/*.spec.ts'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  verbose: true
+}
---
+++ spec/views/cart/cart-item-list.spec.ts
@@ -0,0 +1,7 @@
+import { CartItemList } from '../../../src/views/cart/cart-item-list'
+
+describe(CartItemList, () => {
+  it('should test', () => {
+    expect(1).toBe(1)
+  })
+})
