93eca5a4b019539d57d03aed742725e89e9d418f
philomagi <tooppoo56@gmail.com>
2020-08-01 01:16:24 +0900

    composition api追加

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueCompositionAPI from '@vue/composition-api'
+
 Vue.config.productionTip = false
+
+Vue.use(VueCompositionAPI)
 
 new Vue({
   router,
